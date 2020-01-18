0b6e28fa129bfd3bb549fe343b0eb6518401b6a4
Fatih <akfatih2@gmail.com>
2020-01-18 13:33:07 -0700

    Buttons

--- herohelp/App.js
+++ herohelp/App.js
@@ -1,11 +1,31 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions} from 'react-native';
+import {Button, Icon} from 'react-native-elements';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start !</Text>
-      
+      <Text style={styles.emergency}>Emergency?</Text>
+      <Button
+        title="Overdose"
+        type="outline"
+        buttonStyle = {styles.Button}
+      />
+      <Button
+        title="Heart-attack"
+        type="outline"
+        buttonStyle = {styles.Button}
+      />
+      <Button
+        title="Drowning"
+        type="outline"
+        buttonStyle = {styles.Button}
+      />
+      <Button
+        title="Unconcious"
+        type="outline"
+        buttonStyle = {styles.Button}
+      />
     </View>
   );
 }
@@ -13,8 +33,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    width: '100%',
+    backgroundColor: "#FFFFFF",
     justifyContent: 'center',
+    alignItems: 'center'
   },
+  emergency: {
+
+  },
+  Button: {
+    marginBottom: 20,
+    width: 300
+  }
 });
