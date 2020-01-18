3e166ce3f290e14ffa77857994e602e8299c9ea2
Fatih <akfatih2@gmail.com>
2020-01-18 14:17:55 -0700

    Extending comp

--- herohelp/Screens/login.js
+++ herohelp/Screens/login.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {View,Text} from 'react-native';
 
-export default class LoginScreen{
+export default class LoginScreen extends Component{
     render(){
         return(
             <View>
--- herohelp/Screens/signup.js
+++ herohelp/Screens/signup.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {View,Text} from 'react-native';
 
-export default class SignUpScreen{
+export default class SignUpScreen extends Component{
     render(){
         return(
             <View>
