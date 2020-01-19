8c807f93d9460929a3c0da9c42a0dde31ebce29e
Fatih <akfatih2@gmail.com>
2020-01-18 17:24:12 -0700

    fake url login

--- backend/server.js
+++ backend/server.js
@@ -6,11 +6,14 @@ const https = require('https'); // SSL
 const http = require('http');
 const fs = require('fs'); // For reading example inputs
 const path = require('path');
-// const rfs = require('rotating-file-stream');
 const helmet = require('helmet')
 const {config} = require('./config');
 const server = express();
 const router = express.Router();
+
+const db = require('./db');
+const signUp = require('./routes/signUp.js');
+const login = require('./routes/login.js');
 
 server.use(helmet());
 server.use(bodyParser.urlencoded({ extended: false }));
@@ -18,9 +21,14 @@ server.use(morgan('dev'));
 server.use('/', router);
 server.use(cors());
 
+server.get('/signUp', signUp);
+server.get('/loginin', login);
+
 server.get('*', function(req, res){
     res.status(200).send('There is nothing here. This incident is reported.');
 });
 
-http.createServer(server).listen(80);
-console.log("HTTP server has started on port " + 80);
+db.connect(() => {
+    http.createServer(server).listen(80);
+    console.log("HTTP server has started on port " + 80);
+});
