f0a40617360616d1ac3667c8f19b1a31051640a6
Fatih <akfatih2@gmail.com>
2020-01-19 02:22:05 -0700

    Working warning

--- backend/routes/warning.js
+++ backend/routes/warning.js
@@ -18,6 +18,9 @@ router.get('/warning', function(req, res){
         dbo.collection("customers").find().toArray(function(err, result) {
             if (err) throw err;
             console.log(result);
+
+            var messages = [];
+            var somePushTokens = [];
             result.map(person => {
                 console.log(person.lat);
                 console.log(person.long);
@@ -29,23 +32,62 @@ router.get('/warning', function(req, res){
                 if(currentDist < distanceLIMIT){
                     // Send the push not
                     var pushToken = person.expoToken;
-                    if (!Expo.isExpoPushToken(pushToken)) {
-                        console.error(`Push token ${pushToken} is not a valid Expo push token`);
-                        // continue;
-                    }else{
-                        console.log("Valid token.");
-                    }
-                    var message = {
-                        to: pushToken,
-                        sound: 'default',
-                        body: 'Someone is in danger',
-                        data: { withSome: 'data' },
-                    }
-                    expo.sendPushNotificationsAsync(message);
+                    somePushTokens[somePushTokens.length] = pushToken
+                    
                 }
             });
+            console.log(somePushTokens);
+
+            // Sending
+            for (let pushToken of somePushTokens) {
+                // Each push token looks like ExponentPushToken[xxxxxxxxxxxxxxxxxxxxxx]
+                
+                // Check that all your push tokens appear to be valid Expo push tokens
+                if (!Expo.isExpoPushToken(pushToken)) {
+                    console.error(`Push token ${pushToken} is not a valid Expo push token`);
+                    continue;
+                }
+                
+                // Construct a message (see https://docs.expo.io/versions/latest/guides/push-notifications.html)
+                messages.push({
+                    to: pushToken,
+                    sound: 'default',
+                    body: 'Someone needs ' + req.body.type,
+                    data: { 
+                        long: req.body.long,
+                        lat: req.body.lat,
+                        
+                    },
+                })
+                console.log(messages);
+            }
+
+            let chunks = expo.chunkPushNotifications(messages);
+            let tickets = [];
+            (async () => {
+            // Send the chunks to the Expo push notification service. There are
+            // different strategies you could use. A simple one is to send one chunk at a
+            // time, which nicely spreads the load out over time:
+            for (let chunk of chunks) {
+                try {
+                let ticketChunk = await expo.sendPushNotificationsAsync(chunk);
+                console.log(ticketChunk);
+                tickets.push(...ticketChunk);
+                // NOTE: If a ticket contains an error code in ticket.details.error, you
+                // must handle it appropriately. The error codes are listed in the Expo
+                // documentation:
+                // https://docs.expo.io/versions/latest/guides/push-notifications#response-format
+                } catch (error) {
+                console.error(error);
+                }
+            }
+            })();
+
+
+
             db.close();
         });
+
     });
     res.json({res:true});
 });
